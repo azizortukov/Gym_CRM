0340399932ef62a19101b7e39a9fafca2405a17e
azizortukov <azizortukov818@gmail.com>
2024-08-29 18:03:23 +0500

    config added

--- src/main/java/uz/anas/gymcrm/Main.java
+++ src/main/java/uz/anas/gymcrm/Main.java
@@ -1,8 +1,11 @@
 package uz.anas.gymcrm;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import uz.anas.gymcrm.config.AppConfig;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        var applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
     }
 }
---
+++ src/main/java/uz/anas/gymcrm/config/AppConfig.java
@@ -0,0 +1,12 @@
+package uz.anas.gymcrm.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "uz.anas.gymcrm")
+public class AppConfig {
+
+
+
+}
